8cc277197eacabd3d4bbc0b53e2f026f484f143d
emmanuelbassey685 <emmanuelbassey685@yahoo.com>
2026-09-05 08:00:11 +0300

    Added GET /books route

--- src/router.js
+++ src/router.js
@@ -1,9 +1,13 @@
-// src/router.js
 import express from 'express';
+
 import { getBooksHandler } from './controllers/books.js';
+
+console.log('BOOKS ROUTER LOADED');
 
 const router = express.Router();
 
 router.get('/books', getBooksHandler);
+
+console.log('ROUTES:', router.stack.map(layer => layer.route?.path));
 
 export default router;
