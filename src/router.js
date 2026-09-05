b003bc4eeffffd59c9aa70f9f3f30f1b4aadb8a3
emmanuelbassey685 <emmanuelbassey685@yahoo.com>
2026-09-05 05:50:56 +0300

    Commit

---
+++ src/router.js
@@ -0,0 +1,9 @@
+// src/router.js
+import express from 'express';
+import { getBooksHandler } from './controllers/books.js';
+
+const router = express.Router();
+
+router.get('/books', getBooksHandler);
+
+export default router;
