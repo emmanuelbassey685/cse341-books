52bcfaa63f2b5d3e3a2f27a02d44a0320aa29f9b
emmanuelbassey685 <emmanuelbassey685@yahoo.com>
2026-09-11 16:57:17 +0300

    Complete Book API with author references

--- src/models/books.js
+++ src/models/books.js
@@ -3,15 +3,59 @@ import { getDb } from '../db/connect.js';
 const getAllBooks = async () => {
   const db = getDb();
   const collection = db.collection('books');
-  const books = await collection.find({}).toArray();
-  return books;
+
+  return await collection.find({}).toArray();
 };
 
 const getBookById = async (bookId) => {
   const db = getDb();
   const collection = db.collection('books');
-  const book = await collection.findOne({ id: bookId });
+
+  return await collection.findOne({ id: bookId });
+};
+
+const authorExists = async (authorId) => {
+  const db = getDb();
+  const collection = db.collection('authors');
+
+  const author = await collection.findOne({ id: authorId });
+
+  return !!author;
+};
+
+const createBook = async (book) => {
+  const db = getDb();
+  const collection = db.collection('books');
+
+  await collection.insertOne(book);
+
   return book;
 };
 
-export { getAllBooks, getBookById };
+const updateBook = async (id, book) => {
+  const db = getDb();
+  const collection = db.collection('books');
+
+  await collection.updateOne(
+    { id },
+    { $set: book }
+  );
+
+  return { id, ...book };
+};
+
+const deleteBook = async (id) => {
+  const db = getDb();
+  const collection = db.collection('books');
+
+  return await collection.deleteOne({ id });
+};
+
+export {
+  getAllBooks,
+  getBookById,
+  authorExists,
+  createBook,
+  updateBook,
+  deleteBook
+};
--- swagger.js
+++ swagger.js
@@ -1,4 +1,5 @@
 import { writeFileSync } from 'node:fs';
+
 import swaggerJsdoc from 'swagger-jsdoc';
 
 const options = {
@@ -14,7 +15,38 @@ const options = {
         url: '/',
         description: 'Current server'
       }
-    ]
+    ],
+    components: {
+      schemas: {
+        Book: {
+          type: 'object',
+          required: [
+            'id',
+            'authorId',
+            'title',
+            'publicationDate'
+          ],
+          properties: {
+            id: {
+              type: 'string',
+              example: 'b1'
+            },
+            authorId: {
+              type: 'string',
+              example: 'a1'
+            },
+            title: {
+              type: 'string',
+              example: 'Patterns of Light'
+            },
+            publicationDate: {
+              type: 'string',
+              example: '2021-08-17'
+            }
+          }
+        }
+      }
+    }
   },
   apis: ['./src/router.js', './app.js']
 };
@@ -22,4 +54,5 @@ const options = {
 const swaggerSpec = swaggerJsdoc(options);
 
 writeFileSync('./swagger.json', JSON.stringify(swaggerSpec, null, 2));
+
 console.log('Swagger documentation generated.');
